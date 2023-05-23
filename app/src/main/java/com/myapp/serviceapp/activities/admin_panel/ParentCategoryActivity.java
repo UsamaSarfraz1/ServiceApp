239f5f059f94b99e3cf5630cd59926841b0a82ef
Developer Account <usamasarfraz777>
2023-05-22 19:06:05 -0700

    add parent category add child category

--- app/src/main/java/com/myapp/serviceapp/activities/admin_panel/ParentCategoryActivity.java
+++ app/src/main/java/com/myapp/serviceapp/activities/admin_panel/ParentCategoryActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.myapp.serviceapp.R;
+
 public class ParentCategoryActivity extends AppCompatActivity {
 
     @Override
